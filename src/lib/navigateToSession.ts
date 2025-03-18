6c20e9a8aeaebfc2373267bb17414be77f8db186
Felix Hertweck <github@felix-hertweck.de>
2025-03-18 22:41:26 +0100

    Fix format

--- src/lib/navigateToSession.ts
+++ src/lib/navigateToSession.ts
@@ -1,6 +1,9 @@
 import { NavigateFunction } from "react-router";
 
-export function navigateToSession(sessionId: string | null, navigate: NavigateFunction) {
-    if (sessionId === null) void navigate("/", { replace: true });
-    else void navigate(`/?session=${sessionId}`, { replace: true });
+export function navigateToSession(
+  sessionId: string | null,
+  navigate: NavigateFunction,
+) {
+  if (sessionId === null) void navigate("/", { replace: true });
+  else void navigate(`/?session=${sessionId}`, { replace: true });
 }
