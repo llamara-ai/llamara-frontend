92619049c3119dd34a5880be909289ccf55c040c
Florian Hotze <dev@florianhotze.com>
2025-03-24 13:29:24 +0100

    Add basic PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vite.dev/config/
@@ -7,7 +8,50 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        devOptions: {
+          enabled: process.env.NODE_ENV === "development",
+        },
+        registerType: "autoUpdate",
+        includeAssets: [
+          "favicon.ico",
+          "apple-touch-icon.png",
+          "maskable-icon-512x512.png",
+        ],
+        manifest: {
+          name: "LLAMARA",
+          short_name: "LLAMARA",
+          description: "LLAMARA - Chat with your data",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
