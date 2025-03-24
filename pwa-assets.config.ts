bd5e94fa8d265e73949d855b74b391f8e3aa4a95
Florian Hotze <dev@florianhotze.com>
2025-03-24 17:22:34 +0100

    Format code

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,26 +1,26 @@
 import {
   defineConfig,
-  minimal2023Preset
-} from '@vite-pwa/assets-generator/config'
+  minimal2023Preset,
+} from "@vite-pwa/assets-generator/config";
 
 export default defineConfig({
   headLinkOptions: {
-    preset: '2023'
+    preset: "2023",
   },
   preset: {
     ...minimal2023Preset,
     transparent: {
       sizes: [64, 192, 512],
-      favicons: [[48, 'favicon.ico']],
-      padding: 0.05
+      favicons: [[48, "favicon.ico"]],
+      padding: 0.05,
     },
     maskable: {
       sizes: [512],
-      padding: 0.3
+      padding: 0.3,
     },
     apple: {
       sizes: [180],
-      padding: 0.05
-    }
+      padding: 0.05,
+    },
   },
-})
+});
