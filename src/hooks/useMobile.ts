a7602512fa610beff953593a90a6c4ecffb0b70e
Florian Hotze <dev@florianhotze.com>
2025-03-24 16:52:49 +0100

    Make KnowledgeSourceDetail usable on (mobile) devices with small screens

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -4,14 +4,16 @@ import { useWindowSize } from "usehooks-ts";
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const { width } = useWindowSize({
+  const { width, height } = useWindowSize({
     initializeWithValue: true,
   });
 
-  const [isMobile, setIsMobile] = useState<boolean>(width < MOBILE_BREAKPOINT);
+  const [isMobile, setIsMobile] = useState<boolean>(
+    width < MOBILE_BREAKPOINT || height < MOBILE_BREAKPOINT,
+  );
 
   useEffect(() => {
-    setIsMobile(width < MOBILE_BREAKPOINT);
+    setIsMobile(width < MOBILE_BREAKPOINT || height < MOBILE_BREAKPOINT);
   }, [width]);
 
   return isMobile;
